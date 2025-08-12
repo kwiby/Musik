b20949cdcc08cc863e79c470937aedd9fd193668
kwiby <134721335+kwiby@users.noreply.github.com>
2025-08-11 20:17:39 -0400

    Patch 1.1.1 - Changed tab buttons to be dynamic in position based on screen size instead of having a fixed position.

--- lib/screens/home_screen/tabs/all_music_tab/all_music_tab.dart
+++ lib/screens/home_screen/tabs/all_music_tab/all_music_tab.dart
@@ -32,85 +32,82 @@ class _AllMusicContainerState extends State<AllMusicContainer> {
         ),
 
         // -=-  Tabs  -=-
-        Positioned.fill(
-          bottom: 583,
-          child: Row(
-            spacing: 30, // To keep the position of other boxes constant
-            // while one of them is selected (and therefore bigger), you can
-            // add a variable which is a number that subtracts from the
-            // spacing to keep it the same all the time
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // -=-  All Music Tab  -=-
-              Container(
-                height: 50,
-                width: 80,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  boxShadow: [BoxShadow(
-                    blurRadius: 1,
-                    offset: const Offset(0, -3),
-                    color: Theme.of(context).colorScheme.shadow,
-                  )],
-                ),
-                child: ElevatedButton(
-                  style: const ButtonStyle(
-                    padding: WidgetStatePropertyAll(EdgeInsets.zero),
-                    backgroundColor: WidgetStateColor.transparent,
-                    shadowColor: WidgetStateColor.transparent,
-                  ),
-                  child: Text(
-                    'All Music',
-                    style: TextStyle(
-                      fontFamily: 'SourGummy',
-                      fontVariations: const [FontVariation('wght', 600)],
-                      fontSize: 15,
-                      color: Theme.of(context).colorScheme.tertiary,
-                    ),
-                  ),
-                  onPressed: () {
-                    tabNotifier.value = 'All Music';
-                  },
-                ),
+        Row(
+          spacing: 30, // To keep the position of other boxes constant
+          // while one of them is selected (and therefore bigger), you can
+          // add a variable which is a number that subtracts from the
+          // spacing to keep it the same all the time
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // -=-  All Music Tab  -=-
+            Container(
+              height: 50,
+              width: 80,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                boxShadow: [BoxShadow(
+                  blurRadius: 1,
+                  offset: const Offset(0, -3),
+                  color: Theme.of(context).colorScheme.shadow,
+                )],
               ),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  padding: WidgetStatePropertyAll(EdgeInsets.zero),
+                  backgroundColor: WidgetStateColor.transparent,
+                  shadowColor: WidgetStateColor.transparent,
+                ),
+                child: Text(
+                  'All Music',
+                  style: TextStyle(
+                    fontFamily: 'SourGummy',
+                    fontVariations: const [FontVariation('wght', 600)],
+                    fontSize: 15,
+                    color: Theme.of(context).colorScheme.tertiary,
+                  ),
+                ),
+                onPressed: () {
+                  tabNotifier.value = 'All Music';
+                },
+              ),
+            ),
 
-              // -=-  Playlists Tab  -=-
-              Container(
-                height: 40,
-                width: 80,
-                margin: const EdgeInsets.only(top: 13, bottom: 3),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  boxShadow: [BoxShadow(
-                    blurRadius: 1,
-                    offset: const Offset(0, -3),
-                    color: Theme.of(context).colorScheme.shadow,
-                  )],
-                ),
-                child: ElevatedButton(
-                  style: const ButtonStyle(
-                    padding: WidgetStatePropertyAll(EdgeInsets.zero),
-                    backgroundColor: WidgetStateColor.transparent,
-                    shadowColor: WidgetStateColor.transparent,
-                  ),
-                  child: Text(
-                    'Playlists',
-                    style: TextStyle(
-                      fontFamily: 'SourGummy',
-                      fontVariations: const [FontVariation('wght', 300)],
-                      fontSize: 15,
-                      color: Theme.of(context).colorScheme.tertiary,
-                    ),
-                  ),
-                  onPressed: () {
-                    tabNotifier.value = 'Playlists';
-                  },
-                ),
+            // -=-  Playlists Tab  -=-
+            Container(
+              height: 40,
+              width: 80,
+              margin: const EdgeInsets.only(top: 13, bottom: 3),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                boxShadow: [BoxShadow(
+                  blurRadius: 1,
+                  offset: const Offset(0, -3),
+                  color: Theme.of(context).colorScheme.shadow,
+                )],
               ),
-            ],
-          ),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  padding: WidgetStatePropertyAll(EdgeInsets.zero),
+                  backgroundColor: WidgetStateColor.transparent,
+                  shadowColor: WidgetStateColor.transparent,
+                ),
+                child: Text(
+                  'Playlists',
+                  style: TextStyle(
+                    fontFamily: 'SourGummy',
+                    fontVariations: const [FontVariation('wght', 300)],
+                    fontSize: 15,
+                    color: Theme.of(context).colorScheme.tertiary,
+                  ),
+                ),
+                onPressed: () {
+                  tabNotifier.value = 'Playlists';
+                },
+              ),
+            ),
+          ],
         ),
 
         // -=-  All Music Content Area  -=-
