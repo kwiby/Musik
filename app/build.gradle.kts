56647bc472b20879c469921b284c7374f5b1e2d8
kwiby <134721335+kwiby@users.noreply.github.com>
2026-06-12 20:36:31 -0400

    Patch 1.2.1
    - Update version to match current patch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.musiky"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
