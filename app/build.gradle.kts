727c545f29156eba8e36f7ce2cc5f0a7713f2ccd
kwiby <134721335+kwiby@users.noreply.github.com>
2026-06-22 17:40:12 -0400

    Patch 1.4.1 - Close issue #3 & #4 - Ignore issue #1 - Fix external playback resuming not working - Add system to handle music removing while playing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.musiky"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.4.0"
+        versionCode = 25
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
